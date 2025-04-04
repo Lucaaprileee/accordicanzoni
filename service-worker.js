2c0e4342c8ba1daf9eca0cc6587db5d30e762d6b
Lucaaprileee <Lucaaprile2@gmail.com>
2025-04-04 13:33:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
 const CACHE_NAME = 'accordi-canzoni-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/song-list.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/icon.png',
-  '/icon-512.png'
+    '/accordicanzoni/',
+    '/accordicanzoni/index.html',
+    '/accordicanzoni/song-list.html',
+    '/accordicanzoni/styles.css',
+    '/accordicanzoni/app.js',
+    '/accordicanzoni/manifest.json',
+    '/accordicanzoni/icon.png',
+    '/accordicanzoni/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
 });
